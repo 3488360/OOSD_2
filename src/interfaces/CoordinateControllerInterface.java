1a44f3c4c44d7924a57ed81f64104b16b52831df
Alec McGavin <s3448525@student.rmit.edu.au>
2016-04-14 17:27:42 +1000

    master reset part 1

--- src/interfaces/CoordinateControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import model.Coordinate;
-
-public interface CoordinateControllerInterface {
-//	this class enforces behavior for all coordinate controller
-	
-	public abstract Coordinate getCoordinate(); 
-}
