4bfc73d45cb33515a69325541c9edadd20ad45d4
amcgavin <alec.mcgavin@gmail.com>
2016-05-08 12:58:33 +1000

    re-added piece decorator

---
+++ src/model/pieces/DecoratedPiece.java
@@ -0,0 +1,63 @@
+package model.pieces;
+
+import model.Coordinate;
+
+import java.util.List;
+
+public abstract class DecoratedPiece implements PieceInterface {
+
+    private PieceInterface piece;
+
+    public DecoratedPiece(PieceInterface piece) {
+        this.piece = piece;
+    }
+    @Override
+    public List<Coordinate> getMoves(Coordinate coordinate) {
+        return piece.getMoves(coordinate);
+    }
+
+    @Override
+    public List<Coordinate> getAttackRange(Coordinate coordinate) {
+        return piece.getAttackRange(coordinate);
+    }
+
+    @Override
+    public int getCurrentHealth() {
+        return piece.getCurrentHealth();
+    }
+
+    @Override
+    public int takeDamage(int amount) {
+        return piece.takeDamage(amount);
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return piece.getMaxHealth();
+    }
+
+    @Override
+    public int getStrength() {
+        return piece.getStrength();
+    }
+
+    @Override
+    public int getCost() {
+        return piece.getCost();
+    }
+
+    @Override
+    public String getName() {
+        return piece.getName();
+    }
+
+    @Override
+    public String getPlayerName() {
+        return piece.getPlayerName();
+    }
+
+    @Override
+    public String getIcon() {
+        return piece.getIcon();
+    }
+}
